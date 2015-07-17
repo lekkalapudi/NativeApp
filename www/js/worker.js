88e2844d3f27c1d1563d99574c9bd36fde0565a8
Lekkac <chaithanya.lekkalapudi@gmail.com>
2015-07-17 11:14:35 +0530

    Adding more changes to the files

--- www/js/worker.js
+++ www/js/worker.js
@@ -1,5 +1,5 @@
 importScripts('../lib/socket.io.js');
-var myIoSocket = io.connect('http://localhost:3000');
+var myIoSocket = io.connect('http://inenlekkacl2c.corp.emc.com:3000');
 
 myIoSocket.on('send:time', function (data) {
    self.postMessage(data);
